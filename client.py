2e7656c8e82ba8fcec8aa71ae92c8df176a0edaf
PimonovDaniil <PimonovDaniil@users.noreply.github.com>
2022-05-27 02:27:47 +0700

    Считаем сумму

--- client.py
+++ client.py
@@ -52,3 +52,42 @@ while True:
             time.sleep(1)
     except:
         pass
+
+addr = (myHost, myPort)
+
+# socket - функция создания сокета
+# первый параметр socket_family может быть AF_INET или AF_UNIX
+# второй параметр socket_type может быть SOCK_STREAM(для TCP) или SOCK_DGRAM(для UDP)
+udp_socket = socket(AF_INET, SOCK_DGRAM)
+# bind - связывает адрес и порт с сокетом
+udp_socket.bind(addr)
+
+
+# Бесконечный цикл работы программы
+while True:
+    print('wait data...')
+    # recvfrom - получает UDP сообщения
+    conn, addr = udp_socket.recvfrom(1024)
+    data = json.loads(conn.decode())
+    md5 = hashlib.md5()
+    md5.update(data[1].encode())
+    if md5.hexdigest() == data[0]:
+        # print("Контрольная сумма совпадает")
+        data = json.loads(data[1])
+        print(data)
+        res = []
+        for i in range(len(data[0])):
+            sum = 0
+            for j in range(len(data)):
+                sum += data[j][i]
+            res.append(sum)
+        md5 = hashlib.md5()
+        md5.update(json.dumps(res).encode())
+        # sendto - передача сообщения UDP
+        udp_socket.sendto(json.dumps([md5.hexdigest(), json.dumps(res)]).encode(), addr)
+    else:
+        md5 = hashlib.md5()
+        md5.update(b'False')
+        # sendto - передача сообщения UDP
+        udp_socket.sendto(json.dumps([md5.hexdigest(), 'False']).encode(), addr)
+udp_socket.close()
--- mainClient.py
+++ mainClient.py
@@ -3,8 +3,10 @@ from socket import *
 import sys
 import hashlib
 import json
+import numpy as np
 
 global raschetchiki
+global sumMatrix
 
 
 def getRequest(data, host, port):
@@ -30,7 +32,33 @@ def getRequest(data, host, port):
     else:
         print("Контрольная сумма полученного сообщения не совпадает!")
         raise Exception('Контрольная сумма полученного сообщения не совпадает!')
+    udp_socket.close()
 
+
+def getSum(data, host, port, a, b):
+    md5 = hashlib.md5()
+    md5.update(data.encode())
+    addr = (host, port)
+    udp_socket = socket(AF_INET, SOCK_DGRAM)
+    if not data:
+        udp_socket.close()
+        sys.exit(1)
+    udp_socket.settimeout(1)
+    data = json.dumps([md5.hexdigest(), data])
+    data = str.encode(data)
+    udp_socket.sendto(data, addr)
+    data = udp_socket.recvfrom(1024)
+    data = json.loads(data[0].decode())
+    md5 = hashlib.md5()
+    md5.update(data[1].encode())
+    if md5.hexdigest() == data[0]:
+        print("Контрольная сумма совпадает")
+        print(data[1])
+    #     global raschetchiki
+    #     raschetchiki = json.loads(data[1])
+    # else:
+    #     print("Контрольная сумма полученного сообщения не совпадает!")
+    #     raise Exception('Контрольная сумма полученного сообщения не совпадает!')
     udp_socket.close()
 
 
@@ -48,3 +76,34 @@ while True:
     except:
         pass
 print("Получили данные о доступных расчётчиках " + str(raschetchiki))
+
+# читаем данные
+readData = ""
+with open("data.txt", "r") as f:
+    for line in f:
+        readData += line
+m1, m2 = readData.split("\n\n")
+m1 = np.array(list(map(lambda x: list(map(float, x)), list(map(lambda x: x.split(" "), m1.split("\n"))))))
+m2 = np.array(list(map(lambda x: list(map(float, x)), list(map(lambda x: x.split(" "), m2.split("\n"))))))
+# m1.shape = (1, 12)
+# m1.shape = (3, 4)
+l1 = len(m1)
+l2 = len(m1[0])
+sumMatrix = [0] * (l1 * l2)
+l_raschetchiki = len(raschetchiki)
+if l_raschetchiki > 0:
+    m1.shape = (1, l1 * l2)
+    m2.shape = (1, l1 * l2)
+    m1 = list(m1)[0]
+    m2 = list(m2)[0]
+
+    getSum(json.dumps([list(m1), list(m2)]), raschetchiki[0][0], raschetchiki[0][1], 0, 12)
+    # for i in range(0, l1 * l2, (l1 * l2) // l_raschetchiki):
+    #     if i + l_raschetchiki >= l1 * l2:
+    #         print(m1[i:l1 * l2])
+    #         break
+    #     else:
+    #         print(m1[i:i + ((l1 * l2) // l_raschetchiki)])
+
+else:
+    print("А нет у нас расчётчиков :(")
